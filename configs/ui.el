a10e31b611631b42c15b2dc2a90041a1287b74f8
scientiac <iacsci@proton.me>
2025-04-19 21:46:38 +0545

    remove: org heading render when launched from client
    When the server is started in the background, the fonts arent
    correctly set so it was to fix that problem, now I dont use emacs
    server much.

--- configs/ui.el
+++ configs/ui.el
@@ -9,10 +9,6 @@
 ;; No startup  screen
 (setq inhibit-startup-screen t)
 
-;; No startup message
-(setq inhibit-startup-message t)
-(eval '(setq inhibit-startup-echo-area-message "scientiac"))
-
 ;; No message in scratch buffer
 (setq initial-scratch-message nil)
 
@@ -51,29 +47,16 @@
 
 ;; y/n for  answering yes/no questions
 (fset 'yes-or-no-p 'y-or-n-p)
+(setq use-short-answers t)
 
 ;; Scroll Smoothly
 (setq auto-window-vscroll nil
+	  mouse-wheel-progressive-speed nil
       scroll-margin 1
       scroll-step 4
       scroll-conservatively 10000
       fast-but-imprecise-scrolling nil
       jit-lock-defer-time 0
       scroll-preserve-screen-position 1)
-
-;; Fonts for Org
-(defun iac/org-headings (&optional frame)
-  "Set Org mode heading font sizes, optionally for a specific FRAME."
-  (with-selected-frame (or frame (selected-frame))
-    (custom-set-faces
-     '(org-level-1 ((t (:height 1.5))))
-     '(org-level-2 ((t (:height 1.4))))
-     '(org-level-3 ((t (:height 1.3))))
-     '(org-level-4 ((t (:height 1.2))))
-     '(org-level-5 ((t (:height 1.1)))))))
-
-;; Run on startup and whenever a new frame is created
-(add-hook 'after-init-hook #'iac/org-headings)
-(add-hook 'after-make-frame-functions #'iac/org-headings)
 
 ;;; ui.el ends here.
